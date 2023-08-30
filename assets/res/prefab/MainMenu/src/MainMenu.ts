1a1edcfaa5a0c1875ce0accbc680dd16d607dcbe
尤海涛 <601231596@qq.com>
2023-08-30 09:59:19 +0800

    【提交 更新】 1.主界面ui调整；

--- assets/res/prefab/MainMenu/src/MainMenu.ts
+++ assets/res/prefab/MainMenu/src/MainMenu.ts
@@ -2,7 +2,6 @@ import { kit } from "../../../../src/kit/kit";
 import { PopupCacheMode } from "../../../../src/kit/manager/popupManager/PopupManager";
 import Common from "../../../../src/config/Common";
 import CConst from "../../../../src/config/CConst";
-import DataManager, { LangChars } from "../../../../src/config/DataManager";
 
 const { ccclass, property } = cc._decorator;
 @ccclass
@@ -10,26 +9,47 @@ export default class MainMenu extends cc.Component {
 
     @property(cc.Node) mask: cc.Node = null;
     @property(cc.Node) content: cc.Node = null;
-    @property(cc.Node) nodeTitle: cc.Node = null;
-    @property(cc.Node) nodeUI: cc.Node = null;
-    @property(cc.Node) btnSet: cc.Node = null;
-    @property(cc.Node) labelLevel: cc.Node = null;
+    
+    // 顶部ui
+    @property(cc.Node) uiTop: cc.Node = null;
+    @property(cc.Node) topLabelStrengthNum: cc.Node = null;
+    @property(cc.Node) topLabelStrengthMax: cc.Node = null;
+    @property(cc.Node) topLabelStrengthTime: cc.Node = null;
+    @property(cc.Node) topLabelCoinNum: cc.Node = null;
+    
+    // 底部ui
+    @property(cc.Node) uiBottom: cc.Node = null;
+    @property(cc.Node) bottomLight: cc.Node = null;
+    @property(cc.Node) bottomShop: cc.Node = null;
+    @property(cc.Node) bottomHome: cc.Node = null;
+    @property(cc.Node) bottomTheme: cc.Node = null;
+    
+    // 中间ui
+    @property(cc.Node) uiMid: cc.Node = null;
+    // 中间ui--商店
+    @property(cc.Node) midShop: cc.Node = null;
+    // 中间ui--主菜单
+    @property(cc.Node) midHome: cc.Node = null;
+    // 中间ui--主菜单（顶部）
+    @property(cc.Node) midHomeTop: cc.Node = null;
+    @property(cc.Node) midHomeTopLabel: cc.Node = null;
+    @property(cc.Node) midHomeTopProcess: cc.Node = null;
+    @property(cc.Node) midHomeBottom: cc.Node = null;
+    // 中间ui--主菜单（左侧）
+    @property(cc.Node) midHomeLeft: cc.Node = null;
+    @property(cc.Node) midHomeLeftBoxXing: cc.Node = null;
+    @property(cc.Node) midHomeLeftBoxXingProcess: cc.Node = null;
+    @property(cc.Node) midHomeLeftCalendar: cc.Node = null;
+    // 中间ui--主菜单（右侧）
+    @property(cc.Node) midHomeRight: cc.Node = null;
+    @property(cc.Node) midHomeRightBoxLevel: cc.Node = null;
+    @property(cc.Node) midHomeRightBoxLevelProcess: cc.Node = null;
+    @property(cc.Node) midHomeRightBank: cc.Node = null;
+    // 中间ui--主题
+    @property(cc.Node) midTheme: cc.Node = null;
 
-    async init(callback: Function = null){
+    async init(callback: Function = null) {
         Common.log('MainMenu init()');
-
-        this.mask.setContentSize(cc.winSize);
-        this.content.setContentSize(cc.winSize);
-        this.nodeTitle.y = DataManager.getTitlePosY();
-        this.btnSet.y = cc.winSize.height * 0.5 - 100;
-        
-        this.nodeUI.opacity = 0;
-        cc.tween(this.nodeUI).to(0.383, {opacity: 255}).call(()=>{
-            callback && callback();
-        }).start();
-
-        let chars = await DataManager.getString(LangChars.LEVEL); 
-        this.labelLevel.getComponent(cc.Label).string = chars + ':' + DataManager.data.sortData.level;
     }
 
     /** 按钮事件 设置 */
