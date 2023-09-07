9d8076c6199c9618b2b00f081a8f4292f4b63a90
尤海涛 <601231596@qq.com>
2023-09-07 15:26:54 +0800

    【提交 更新】： 1.本地化文字配置；

---
+++ assets/prefabs/Popup/BoxXingxing/src/BoxXingxing.ts
@@ -0,0 +1,36 @@
+import { kit } from "../../../../src/kit/kit";
+import PopupBase from "../../../../src/kit/manager/popupManager/PopupBase";
+import CConst from "../../../../src/config/CConst";
+import DataManager, { LangChars } from "../../../../src/config/DataManager";
+
+const { ccclass, property } = cc._decorator;
+@ccclass
+export default class BoxXingxing extends PopupBase {
+
+    @property(cc.Node) itemLabelTitle: cc.Node = null;
+    @property(cc.Node) itemLabelMid: cc.Node = null;
+    @property(cc.Node) itemLabelPlay: cc.Node = null;
+
+    protected onLoad(): void {
+        this.initLabel();
+    }
+
+    initLabel(){
+        // DataManager.setString(LangChars.QUIT, (chars: string)=>{
+        //     this.itemLabelMid.getComponent(cc.Label).string = chars + '?';
+        // });
+        // DataManager.setString(LangChars.OK, (chars: string)=>{
+        //     this.itemLabelPlay.getComponent(cc.Label).string = chars;
+        // });
+    }
+
+    eventBtnSure() {
+        kit.Audio.playEffect(CConst.sound_clickUI);
+        kit.Popup.hide();
+    }
+
+    eventBtnExit() {
+        kit.Audio.playEffect(CConst.sound_clickUI);
+        kit.Popup.hide();
+    }
+}
--- assets/src/config/DataManager.ts
+++ assets/src/config/DataManager.ts
@@ -20,35 +20,142 @@ export const LangFile = {
 
 /** 本地化 文字 字段 */
 export const LangChars = {
-    Loading: "Loading",
-    sort1: "sort1",
-    sort2: "sort2",
-    sort3: "sort3",
-    SPECIAL: "SPECIAL",
-    REVIEW: "REVIEW",
-    PleaseRateUs: "PleaseRateUs",// 未使用
-    CannotWatchAds: "CannotWatchAds",
-    LEVEL: "LEVEL",
-    QUIT: "QUIT",
-    OK: "OK",
-    SETTING: "SETTING",
-    ON: "ON",
-    OFF: "OFF",
-    SPECIALLEVEL: "SPECIALLEVEL",
-    PLAY: "PLAY",
-    SKIP: "SKIP"
-};
-
-/** 本地化 资源类型 */
-export const ResType = {
-    PNG: "png",
-    DRAGON: "dragon",
+    loading: "loading",
+    guide_tip: "guide_tip",
+    guide_back: "guide_back",
+    guide_refresh: "guide_refresh",
+    guide_ice: "guide_ice",
+    guide_magnet: "guide_magnet",
+    guide_clock: "guide_clock",
+    over_timeout_title: "over_timeout_title",
+    over_timeout_desc: "over_timeout_desc",
+    over_timeout_continue: "over_timeout_continue",
+    over_timeout_giveup: "over_timeout_giveup",
+    over_nospace_desc: "over_nospace_desc",
+    over_nospace_gobackgood: "over_nospace_gobackgood",
+    exit_confirm_desc: "exit_confirm_desc",
+    exit_confirm_restart: "exit_confirm_restart",
+    exit_confirm_giveup: "exit_confirm_giveup",
+    pause_desc: "pause_desc",
+    pause_resume: "pause_resume",
+    pause_quit: "pause_quit",
+    exit_title: "exit_title",
+    exit_desc: "exit_desc",
+    exit_resume: "exit_resume",
+    exit_quit: "exit_quit",
+    win_finish: "win_finish",
+    win_next: "win_next",
+    hour: "hour",
+    minite: "minite",
+    second: "second",
+    Level: "Level",
+    Bank: "Bank",
+    Home: "Home",
+    Shop: "Shop",
+    Collection: "Collection",
+    setting_title: "setting_title",
+    setting_language: "setting_language",
+    before_level: "before_level",
+    gameBefore_wins: "gameBefore_wins",
+    gameBefore_play: "gameBefore_play",
+    gameBefore_chose: "gameBefore_chose",
+    continuesChallenge_title: "continuesChallenge_title",
+    continuesChallenge_desc: "continuesChallenge_desc",
+    continuesChallenge_times: "continuesChallenge_times",
+    boxLevel_title: "boxLevel_title",
+    boxLevel_desc: "boxLevel_desc",
+    boxLevel_continue: "boxLevel_continue",
+    boxXingxing_title: "boxXingxing_title",
+    boxXingxing_desc: "boxXingxing_desc",
+    boxXingxing_continue: "boxXingxing_continue",
+    tapToClaim: "tapToClaim",
+    tapToContinue: "tapToContinue",
+    goldCollection: "goldCollection",
+    addCoin_title: "addCoin_title",
+    addCoin_desc_0: "addCoin_desc_0",
+    addCoin_desc_1: "addCoin_desc_1",
+    addCoin_watch: "addCoin_watch",
+    addLife_title: "addLife_title",
+    addLife_desc: "addLife_desc",
+    addLife_more: "addLife_more",
+    addLife_nextFill: "addLife_nextFill",
+    addLife_reFill: "addLife_reFill",
+    bank_title: "bank_title",
+    bank_desc: "bank_desc",
+    SHOP: "SHOP",
+    BeginnersBundle: "BeginnersBundle",
+    ElfGiftBox: "ElfGiftBox",
+    PrincessTreasure: "PrincessTreasure",
+    ShiningBundle: "ShiningBundle",
+    MikoGiftPack: "MikoGiftPack",
+    QueenTreasure: "QueenTreasure",
+    LimitedOffer: "LimitedOffer",
+    RemoveAds: "RemoveAds",
+    MostPopular: "MostPopular",
+    BestPrice: "BestPrice",
+    Goods: "Goods",
+    Moment: "Moment",
+    Doll: "Doll",
+    Handbag: "Handbag",
+    Satchel: "Satchel",
+    Ball: "Ball",
+    Christmas: "Christmas",
+    shoes: "shoes",
+    Cap: "Cap",
+    Flower: "Flower",
+    Electronics: "Electronics",
+    GreenPlants: "GreenPlants",
+    Toy: "Toy",
+    adsNo: "adsNo",
+    adsRemovePopup: "adsRemovePopup",
+    adsNetworkNo: "adsNetworkNo",
+    adsNetworkElse: "adsNetworkElse",
+    adsNotReady: "adsNotReady",
+    dailyChallenge_January: "dailyChallenge_January",
+    dailyChallenge_February: "dailyChallenge_February",
+    dailyChallenge_March: "dailyChallenge_March",
+    dailyChallenge_April: "dailyChallenge_April",
+    dailyChallenge_May: "dailyChallenge_May",
+    dailyChallenge_June: "dailyChallenge_June",
+    dailyChallenge_July: "dailyChallenge_July",
+    dailyChallenge_August: "dailyChallenge_August",
+    dailyChallenge_September: "dailyChallenge_September",
+    dailyChallenge_October: "dailyChallenge_October",
+    dailyChallenge_November: "dailyChallenge_November",
+    dailyChallenge_December: "dailyChallenge_December",
+    dailyChallenge_Monday: "dailyChallenge_Monday",
+    dailyChallenge_Tuesday: "dailyChallenge_Tuesday",
+    dailyChallenge_Wednesday: "dailyChallenge_Wednesday",
+    dailyChallenge_Thursday: "dailyChallenge_Thursday",
+    dailyChallenge_Friday: "dailyChallenge_Friday",
+    dailyChallenge_Saturday: "dailyChallenge_Saturday",
+    dailyChallenge_Sunday: "dailyChallenge_Sunday",
+    dailyChallenge_play: "dailyChallenge_play",
+    dailyChallenge_win_1: "dailyChallenge_win_1",
+    dailyChallenge_win_3: "dailyChallenge_win_3",
+    dailyChallenge_win_10: "dailyChallenge_win_10",
+    dailyChallenge_win_28: "dailyChallenge_win_28",
+    logon_reward_daily: "logon_reward_daily",
+    logon_reward_claim: "logon_reward_claim",
+    logon_reward_day_1: "logon_reward_day_1",
+    logon_reward_day_2: "logon_reward_day_2",
+    logon_reward_day_3: "logon_reward_day_3",
+    logon_reward_day_4: "logon_reward_day_4",
+    logon_reward_day_5: "logon_reward_day_5",
+    logon_reward_day_6: "logon_reward_day_6",
+    logon_reward_day_7: "logon_reward_day_7"
 };
 
 /** 本地化 图片 名字 */
 export const LangImg = {
     word_logo_title: "word_logo_title",
     word_win_title: "word_win_title",
+};
+
+/** 资源类型 */
+export enum TypeRes {
+    PNG = "png",// 图片
+    DRAGON = "dragon", // 龙骨
 };
 
 /** 游戏状态 */
@@ -58,7 +165,7 @@ export enum StateGame {
     game = 1 << 2,// 返回上一步
 }
 
-/** 道具 */
+/** 道具类型 */
 export enum TypeProp {
     ice = 1,// 冰冻
     tip = 1 << 1,// 提示
@@ -383,7 +490,7 @@ class DataManager {
     /** 获取字符串 */
     public setString(key: string, callback) {
         let resPath = CConst.pathLanguage + this.langCur;
-        kit.Resources.loadRes(CConst.bundleCommon, resPath, cc.JsonAsset, (e: any, jsonAsset: cc.JsonAsset)=>{
+        kit.Resources.loadRes(CConst.bundleCommon, resPath, cc.JsonAsset, (e: any, jsonAsset: cc.JsonAsset) => {
             if (jsonAsset) {
                 callback && callback(jsonAsset.json[key]);
             }
